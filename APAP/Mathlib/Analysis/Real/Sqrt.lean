1e2ce8c5cffd813052b03804c18deadac2f65cde
√2 <240895760+sqrt-of-2@users.noreply.github.com>
2026-07-01 12:12:30 +0200

    feat: prove `Real.sqrt_le_self.mp` (#21)

--- APAP/Mathlib/Analysis/Real/Sqrt.lean
+++ APAP/Mathlib/Analysis/Real/Sqrt.lean
@@ -7,16 +7,14 @@ public section
 namespace Real
 variable {x : ℝ}
 
-@[simp] lemma sqrt_le_self : √x ≤ x ↔ x = 0 ∨ 1 ≤ x where
-  mp := sorry
-  mpr := by
-    rintro (rfl | hx)
-    · simp
-    · exact sqrt_le_iff.2 ⟨zero_le_one.trans hx, le_self_pow₀ hx two_ne_zero⟩
+@[simp] lemma sqrt_le_self : √x ≤ x ↔ x = 0 ∨ 1 ≤ x := by
+  obtain hx | hx := le_or_gt x 0
+  · simp [sqrt_eq_zero_of_nonpos hx, le_antisymm_iff, hx, not_le.2 (hx.trans_lt zero_lt_one)]
+  · simp [sqrt_le_iff, sq, le_mul_iff_one_le_right hx, hx.le, hx.ne']
 
 @[simp] lemma le_sqrt_self : x ≤ √x ↔ x ≤ 1 := by
   obtain hx | hx := le_or_gt x 0
-  · exact iff_of_true (hx.trans x.sqrt_nonneg) (hx.trans zero_le_one)
+  · simp [hx.trans]
   · rw [le_sqrt' hx, sq, mul_le_iff_le_one_left hx]
 
 end Real
